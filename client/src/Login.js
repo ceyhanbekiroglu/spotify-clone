725fe6e325489c9efb6f26f5f14706a14f19326a
ceyhan bekiroglu <ceyhanbekiroglu@gmail.com>
2022-06-04 23:33:54 +0200

    Change correct `client_id`

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,7 +1,11 @@
 import React from 'react'
+
+import { Container } from 'react-bootstrap'
+
 const AUTH_URL =
-	'https://accounts.spotify.com/authorize?client_id=8b945ef10ea24755b83ac50cede405a0&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
+	'https://accounts.spotify.com/authorize?client_id=ee3cfbab67ac45559416505321a9fe7f&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
 
 export default function Login() {
-	return <div>Login</div>
+	return
+	;<div>Login</div>
 }
