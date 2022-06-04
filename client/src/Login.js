d64c721ca3a8ad85b9a58a2e4866531219d0b0fc
ceyhan bekiroglu <ceyhanbekiroglu@gmail.com>
2022-06-04 23:42:28 +0200

    Add container

--- client/src/Login.js
+++ client/src/Login.js
@@ -6,6 +6,11 @@ const AUTH_URL =
 	'https://accounts.spotify.com/authorize?client_id=ee3cfbab67ac45559416505321a9fe7f&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
 
 export default function Login() {
-	return
-	;<div>Login</div>
+	return (
+		<Container>
+			<a className='btn btn-success btn-lg' href={AUTH_URL}>
+				Login with Spotify
+			</a>
+		</Container>
+	)
 }
