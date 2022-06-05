94597157d2661f49666490aee6135a887a30461a
ceyhan bekiroglu <ceyhanbekiroglu@gmail.com>
2022-06-05 23:24:43 +0200

    Add `Login` button

--- client/src/Login.js
+++ client/src/Login.js
@@ -7,7 +7,10 @@ const AUTH_URL =
 
 export default function Login() {
 	return (
-		<Container>
+		<Container
+			className='d-flex justify-content-center align-items-center'
+			style={{ minHeight: '100vh' }}
+		>
 			<a className='btn btn-success btn-lg' href={AUTH_URL}>
 				Login with Spotify
 			</a>
