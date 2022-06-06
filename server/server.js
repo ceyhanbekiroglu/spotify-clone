c3ffbd598e60ba0a8d8f52f7e8cdc44dfbd790db
ceyhan bekiroglu <ceyhanbekiroglu@gmail.com>
2022-06-06 23:28:07 +0200

    Add `spotifyApi` authorization

--- server/server.js
+++ server/server.js
@@ -4,9 +4,21 @@ const spotifyWebApi = require('spotify-web-api-node')
 const app = express()
 
 app.post('/login', (req, res) => {
+	const code = req.body.code
 	const spotifyApi = new spotifyWebApi({
-		redirectUri: 'localhost:3000',
-		clientId: '',
-		clientSecret: '',
+		redirectUri: 'http://localhost:3000',
 	})
+
+	spotifyApi
+		.authorizationCodeGrant(code)
+		.then((data) => {
+			res.json({
+				accessToken: data.body.access_token,
+				refreshToken: data.body.refresh_token,
+				expiresIn: data.body.expires_in,
+			})
+		})
+		.catch(() => {
+			res.sendStatus(400)
+		})
 })
