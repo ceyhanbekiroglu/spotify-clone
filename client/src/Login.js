16e4693e955c2ec80891155a46df9ffcf1e28fae
ceyhan bekiroglu <ceyhanbekiroglu@gmail.com>
2022-06-06 23:14:40 +0200

    Fix for account entry link

--- client/src/Login.js
+++ client/src/Login.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 
 const AUTH_URL =
-	'https://accounts.spotify.com/authorize?client_id=ee3cfbab67ac45559416505321a9fe7f&response_type=code&redirect_uri=localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
+	'https://accounts.spotify.com/authorize?client_id=ee3cfbab67ac45559416505321a9fe7f&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
 
 export default function Login() {
 	return (
