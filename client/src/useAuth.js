efbdc19a8ace77fb68f798134c50e388f6662572
ceyhan bekiroglu <ceyhanbekiroglu@gmail.com>
2022-06-17 22:45:28 +0200

    Add `refreshToken` and `expriresIn`

--- client/src/useAuth.js
+++ client/src/useAuth.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
 export default function useAuth(code) {
-	const [accessToken, setAccessToken] = React.useState(null)
+	const [accessToken, setAccessToken] = React.useState()
+	const [refreshToken, setRefreshToken] = React.useState()
+	const [expiresIn, setExpiresIn] = React.useState()
 }
