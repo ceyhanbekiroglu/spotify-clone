242ea0ce76487c82487bc31562b93056a3b6e220
ceyhan bekiroglu <ceyhanbekiroglu@gmail.com>
2022-06-20 23:52:53 +0200

    Add .env file

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const spotifyWebApi = require('spotify-web-api-node')
 
 const app = express()
+app.use(cors())
+app.use(bodyParser.json())
 
-app.post('/login', (req, res) => {
+app.post('/refresh', (req, res) => {
+	const refreshToken = req.body.refreshTokenS
+})
+
+app.s('/login', (req, res) => {
 	const code = req.body.code
-	const spotifyApi = new spotifyWebApi({
-		redirectUri: 'http://localhost:3000',
+	const spotifyApi = new SpotifyWebApi({
+		redirectUri: process.env.REDIRECT_URI,
+		clientId: process.env.CLIENT_ID,
+		clientSecret: process.env.CLIENT_SECRET,
+		refreshToken,
 	})
 
 	spotifyApi
@@ -18,7 +29,10 @@ app.post('/login', (req, res) => {
 				expiresIn: data.body.expires_in,
 			})
 		})
-		.catch(() => {
+		.catch((err) => {
+			console.log(err)
 			res.sendStatus(400)
 		})
 })
+
+app.listen(3001)
