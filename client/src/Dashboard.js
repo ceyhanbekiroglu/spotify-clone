d72e6d8c8df4ce36a4bd1e0da8c3ff6b617b75b3
ceyhan bekiroglu <ceyhanbekiroglu@gmail.com>
2022-07-06 23:02:33 +0200

    Continue with calcel

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -45,6 +45,17 @@ export default function Dashboard({ code }) {
 	if (!accessToken) return
 	
 	let cancel = false
+	spotifyApi.searchTracks(search).then(res => {
+      if (cancel) return
+      setSearchResults(
+        res.body.tracks.items.map(track => {
+          const smallestAlbumImage = track.album.images.reduce(
+            (smallest, image) => {
+              if (image.height < smallest.height) return image
+              return smallest
+            },
+            track.album.images[0]
+		  )
 
 	return (
 		<Container className='d-flex flex-column py-2' style={{ height: '100vh' }}>
