00ed13456a3c717c0fa1d69c24e1b594dcdd0dbb
ceyhan bekiroglu <ceyhanbekiroglu@gmail.com>
2022-07-08 23:27:11 +0200

    Add return for calcel

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -41,31 +41,33 @@ export default function Dashboard({ code }) {
 	}, [accessToken])
 
 	useEffect(() => {
-    if (!search) return setSearchResults([])
-	if (!accessToken) return
-	
-	let cancel = false
-    spotifyApi.searchTracks(search).then(res => {
-      if (cancel) return
-      setSearchResults(
-        res.body.tracks.items.map(track => {
-          const smallestAlbumImage = track.album.images.reduce(
-            (smallest, image) => {
-              if (image.height < smallest.height) return image
-              return smallest
-            },
-            track.album.images[0]
-          )
+		if (!search) return setSearchResults([])
+		if (!accessToken) return
 
-          return {
-            artist: track.artists[0].name,
-            title: track.name,
-            uri: track.uri,
-            albumUrl: smallestAlbumImage.url,
-          }
-        })
-      )
-    })
+		let cancel = false
+		spotifyApi.searchTracks(search).then((res) => {
+			if (cancel) return
+			setSearchResults(
+				res.body.tracks.items.map((track) => {
+					const smallestAlbumImage = track.album.images.reduce(
+						(smallest, image) => {
+							if (image.height < smallest.height) return image
+							return smallest
+						},
+						track.album.images[0]
+					)
+
+					return {
+						artist: track.artists[0].name,
+						title: track.name,
+						uri: track.uri,
+						albumUrl: smallestAlbumImage.url,
+					}
+				})
+			)
+		})
+		return () => (cancel = true)
+	}, [search, accessToken])
 
 	return (
 		<Container className='d-flex flex-column py-2' style={{ height: '100vh' }}>
