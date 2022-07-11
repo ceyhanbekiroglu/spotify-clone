d91e0d5ccb2eecf75a23ca98a659dc07b0f59a82
ceyhan bekiroglu <ceyhanbekiroglu@gmail.com>
2022-07-11 20:19:19 +0000

    Add function Player

--- client/src/Player.js
+++ client/src/Player.js
@@ -1,2 +1,6 @@
 import { useState, useEffect } from 'react'
 import SpotifyPlayer from 'react-spotify-web-playback'
+
+export default function Player({ accessToken, trackUri }) {
+	const [play, setPlay] = useState(false)
+}
